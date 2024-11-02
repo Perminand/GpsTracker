9f4fb782aefb5c0224af075419b6ff0895fb5232
Андрей <Perminand@yandex.ru>
2024-11-02 15:51:31 +0300

    feat: Create class

---
+++ src/main/java/ru/perminov/carpool/carpoolApplication.java
@@ -0,0 +1,11 @@
+package ru.perminov.carpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class carpoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(carpoolApplication.class, args);
+    }
+}
