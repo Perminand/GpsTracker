948c21a0f70ee5093cc6a103fd455f7f35ccc3f6
Андрей <Perminand@yandex.ru>
2024-11-02 20:07:38 +0300

    feat: Сделать валидацию

--- src/main/java/ru/perminov/carpool/dto/UserDto.java
+++ src/main/java/ru/perminov/carpool/dto/UserDto.java
@@ -4,14 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.perminov.carpool.model.Role;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+
+    @NotBlank
+    @Min(3)
     private String username;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @Min(6)
     private String password;
+    private List<Role> roles;
+
 }
