e760eb0937013ac80f984ea871838b110c916be0
Андрей <Perminand@yandex.ru>
2024-11-03 06:16:56 +0300

    feat: Create class

--- src/main/java/ru/perminov/carpool/repository/UserRepository.java
+++ src/main/java/ru/perminov/carpool/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.perminov.carpool.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import ru.perminov.carpool.model.User;
 
 public interface UserRepository extends JpaRepository<Long, User> {
